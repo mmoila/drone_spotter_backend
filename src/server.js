df44caf16363ce4e3fd69f9203fd056f70aea334
Mikael Moilanen <mikael.moilanen@gmail.com>
2022-12-21 20:18:07 +0200

    Fix event emitter  memory leak when constantly refreshing web page

--- src/server.js
+++ src/server.js
@@ -8,10 +8,13 @@ const wss = new WebSocket.Server({ server: server })
 
 wss.on("connection", (ws) => {
   console.log("Websocket connection established")
+  emitter.removeAllListeners()
+
   emitter.on("DronesUpdated", async () => {
     const drones = await Drone.find({})
     ws.send(JSON.stringify(drones))
   })
+
   ws.on("message", () => {
     console.log("message received")
   })
