a647d6bbd4180131f9991407ae9ae09211b86825
Mikael Moilanen <mikael.moilanen@gmail.com>
2023-01-01 19:15:33 +0200

    Fix faulty server import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const config = require("./utils/config")
-const server = require("./server").default
+const server = require("./server")
 
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
